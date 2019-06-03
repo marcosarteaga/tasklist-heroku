49479a6fddb4c84f6747bf9b4c12fb724429d92d
marcosarteaga <marcos118118@gmail.com>
2019-06-03 19:35:39 +0200

    estilos

--- web/index.php
+++ web/index.php
@@ -1,3 +1,8 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 <?php
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
@@ -12,10 +17,10 @@ $stmt = $pdo->query("SELECT * FROM tareas");
 echo "<h1>Lista de Tareas</h1>";
 echo"<form method='POST' action='/'>";
 echo "&nbsp";
-echo"<label>Nueva Tarea</label>";
-echo "<input type='text' name='tareaNueva'>";
+echo"<label>Añadir Tarea</label>";
+echo "<input type='text' class='form-control' name='tareaNueva'>";
 echo "&nbsp";
-echo"<input type='submit' value='Insertar' name='botonInsertar'>";	
+echo"<input type='submit' class='btn btn-success' value='Añadir' name='botonInsertar'>";	
 echo "</form>";
 if (isset($_POST['botonInsertar'])) {
     $nuevaTarea1=$_POST['tareaNueva'];
@@ -31,7 +36,7 @@ while ($row = $stmt->fetch()) {
 	echo "<li>";
     echo $row['name'];
     echo "&nbsp";
-    echo"<input type='button' value='Eliminar' name='botonEliminar' onclick=location.href='/?id=".$row['id']."'         >";
+    echo"<input type='button' class='btn btn-warning' value='Eliminar' name='botonEliminar' onclick=location.href='/?id=".$row['id']."'         >";
     echo "</li>";
 }
 echo "</ul>";

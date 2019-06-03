994fbda0993f1411a693bd385da6e0a0d81e2871
marcosarteaga <marcos118118@gmail.com>
2019-06-03 19:46:37 +0200

    style

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+
 <?php
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
@@ -18,7 +19,7 @@ echo "<h1>Lista de Tareas</h1>";
 echo"<form method='POST' action='/'>";
 echo "&nbsp";
 echo"<label>Añadir Tarea</label>";
-echo "<input type='text' class='form-control' name='tareaNueva'>";
+echo "<input type='text' style=' width: 30% ' class='form-control' name='tareaNueva'>";
 echo "&nbsp";
 echo"<input type='submit' class='btn btn-success' value='Añadir' name='botonInsertar'>";	
 echo "</form>";
@@ -30,16 +31,16 @@ if (isset($_POST['botonInsertar'])) {
     $sentencia->execute();
     header("Location: /");
 }
-echo"<ul>";
+echo"<ol>";
 echo"<form method='POST' action='/'>";
 while ($row = $stmt->fetch()) {
 	echo "<li>";
     echo $row['name'];
     echo "&nbsp";
-    echo"<input type='button' class='btn btn-warning' value='Eliminar' name='botonEliminar' onclick=location.href='/?id=".$row['id']."'         >";
+    echo"<input type='button' class='btn btn-danger' value='Eliminar' name='botonEliminar' onclick=location.href='/?id=".$row['id']."'         >";
     echo "</li>";
 }
-echo "</ul>";
+echo "</ol>";
 echo "</form>";
 $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $id=explode('=', $url);
